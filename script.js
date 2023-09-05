08c18a9e05caaa47f6a2d5f775c16a7c32f84738
unknown <mmiq69@gmail.com>
2023-09-05 10:42:47 -0400

    adding onlick in html

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+//  when button is cliked series of prompt should be shown for password criteria
+// we add onclick atribute to button
+
+// first step password length
+
+// second type of charachter 
+
+// third password should be generated
+
+//  last password hsould be shown in alert or windoes.
+
+
